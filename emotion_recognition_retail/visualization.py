8a74c15b5b5d1d00b7fdd5e090db98a0b4c4dfb4
Alexander Slavutin <alexander.slavutin@xperience.ai>
2021-01-07 23:43:28 +0300

    demo changed

--- emotion_recognition_retail/visualization.py
+++ emotion_recognition_retail/visualization.py
@@ -3,7 +3,7 @@ from typing import List
 import cv2
 import numpy as np
 from modelplace_api import AgeGenderLabel
-from modelplace_api.visualization import RGB_COLORS, draw_classification_legend
+from modelplace_api.visualization import RGB_COLORS, draw_classification_legend, draw_text
 
 
 def draw_emotion_recognition_result(
@@ -18,16 +18,18 @@ def draw_emotion_recognition_result(
             RGB_COLORS[196],
             thickness=8,
         )
-        classification_labels = {
-            label.class_name.capitalize(): round(float(label.score), 2)
-            for label in detection.emotion
-        }
-        image = draw_classification_legend(
-            image,
-            classification_labels,
-            font_scale=0.75,
-            font_thickness=2,
-            height_offset=20,
-            cell_height=50,
-        )
+        # THis should be a function in modelplace_api visualization
+        bg_color = RGB_COLORS[196]
+        font_scale = 1
+        font = cv2.FONT_HERSHEY_COMPLEX
+        label_score, label_class_name = detection.emotion[0]
+        (text_width, text_height) = cv2.getTextSize(label_class_name[1], font, fontScale=font_scale, thickness=1)[0]
+        text_offset_x = detection.bbox.x1
+        text_offset_y = detection.bbox.y1 - 15
+        box_coords = ((int(text_offset_x), int(text_offset_y)), (int(text_offset_x + text_width + 2), int(text_offset_y - text_height - 2)))
+        cv2.rectangle(image, box_coords[0], box_coords[1], bg_color, cv2.FILLED)
+        cv2.putText(image, label_class_name[1],
+                    (int(text_offset_x), int(text_offset_y)),
+                    font, fontScale=font_scale,
+                    color=(255, 255, 255), thickness=1)
     return image
