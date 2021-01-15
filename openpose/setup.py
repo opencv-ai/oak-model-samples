342bbf5cd796325f394823e2da26e1a3cf0b6d53
Alexander Slavutin <alexander.slavutin@xperience.ai>
2021-01-15 16:01:40 +0300

    fixed version

--- openpose/setup.py
+++ openpose/setup.py
@@ -32,7 +32,7 @@ setup_kwargs = {
     "python_requires": ">=3.7,<4.0",
     "install_requires": [
         "depthai@git+https://github.com/luxonis/depthai-python@gen2_develop#depthai-0.2.0",
-        "modelplace-api[vis]@git+https://github.com/opencv-ai/modelplace-api.git#egg=modelplace-api-0.2.0",
+        "modelplace-api[vis]@git+https://github.com/opencv-ai/modelplace-api.git@v0.3.0#egg=modelplace-api",
     ],
 }
 
