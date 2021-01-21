37d280a6c00b09a27fa077100f01a8ccc2a20d51
Alexander Slavutin <alexander.slavutin@xperience.ai>
2021-01-21 15:35:16 +0300

    test added

---
+++ yolov4_tiny/tests/test_openvino_tiny_yolo_v4.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+import pydantic
+from modelplace_api import Device
+from modelplace_api.utils import is_equal
+from PIL import Image
+
+from yolov4_tiny import InferenceModel
+
+dir_name = os.path.abspath(os.path.dirname(__file__))
+model_path = os.path.join(os.path.dirname(dir_name), "checkpoint")
+test_image_path = os.path.join(dir_name, "yolov4_tiny_test.jpg")
+test_result_path = os.path.join(dir_name, "openvino_yolov4_tiny_gt.json")
+
+test_image = Image.open(test_image_path).convert("RGB")
+with open(test_result_path, "r") as j_file:
+    test_result = json.loads(j_file.read())
+
+
+def test_process_sample_tiny_yolov4():
+    model = InferenceModel(model_path=model_path, threshold=0.5)
+    model.model_load()
+    model.to_device(Device.cpu)
+    ret = model.process_sample(test_image)
+    ret = [pydantic.json.pydantic_encoder(item) for item in ret]
+    assert is_equal(ret, test_result)
