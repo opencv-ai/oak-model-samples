a72c24d6bcbc200887bd106440f54ffd1e32bd2c
Andrey Senyaev <andrey.senyaev@xperience.ai>
2021-01-22 13:46:49 +0000

    Jsons for new models

--- yolov4_tiny/tests/test_openvino_tiny_yolo_v4.py
+++ yolov4_tiny/tests/test_openvino_tiny_yolo_v4.py
@@ -6,19 +6,20 @@ from modelplace_api import Device
 from modelplace_api.utils import is_equal
 from PIL import Image
 
+from test_utils import reset_ports
 from yolov4_tiny import InferenceModel
 
 dir_name = os.path.abspath(os.path.dirname(__file__))
 model_path = os.path.join(os.path.dirname(dir_name), "checkpoint")
 test_image_path = os.path.join(dir_name, "yolov4_tiny_test.jpg")
-test_result_path = os.path.join(dir_name, "yolov4_tiny_gt.json.json")
+test_result_path = os.path.join(dir_name, "yolov4_tiny_gt.json")
 
 test_image = Image.open(test_image_path).convert("RGB")
 with open(test_result_path, "r") as j_file:
     test_result = json.loads(j_file.read())
 
 
-def test_process_sample_tiny_yolov4():
+def test_process_sample_tiny_yolov4(reset_ports):
     model = InferenceModel(model_path=model_path, threshold=0.5)
     model.model_load()
     model.to_device(Device.cpu)
